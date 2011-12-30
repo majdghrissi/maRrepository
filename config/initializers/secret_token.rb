5745c9005fe7065dacc7325ca12da97be27d2642
gem install mysql <www.majd@hotmail.com>
2011-12-30 09:41:17 +0000

    save1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site1::Application.config.secret_token = '01ab8ca294ee9f6bdded7295082c3da14b8c6572c8b5d81c4611cdaacab38b8018bbe09ac2e6ca5ebbb5908a644d5f2bc49dcb38d0f221a23f8fca064230e69e'
